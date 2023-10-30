90d5b7a70a40698e87119bdbe73ec21d6aec678b
Safaa Khalil <khalilsafaa@gmail.com>
2023-10-30 00:58:29 -0400

    firstuuddd

--- client/src/pages/AdminPage.js
+++ client/src/pages/AdminPage.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Entry from "../components/Entry";
 
 const AdminPage = () => {
   const xxx = [{ email: "asdasd@gmail.com", password: "admooed443#" }];
+  const [data, setData] = useState([{}])
+  const getInfo = async () => {
+    const res = await axios.get(`${uri}/api/info`).then(setData(res.data))
+  }
   return (
     <div className="admin-page">
       <h1>Admin Panel</h1>
+      <button onClick={() => getInfo()}>Refresh</button>
       <hr />
       <div className="accounts">
-        {xxx.map((e, i) => {
+        {data.map((e, i) => {
           return (
             <Entry
               key={`id${i + 1}`}
